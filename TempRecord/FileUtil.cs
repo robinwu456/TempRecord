87ad68ce1d70c14d8e741e2ef1da2930980453c2
robin <robinwu456@yahoo.com.tw>
2021-12-30 15:59:39 +0800

    加入專案檔案。

---
+++ TempRecord/FileUtil.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+using System.Globalization;
+using System.Runtime.InteropServices;
+
+public static class FileUtil {
+    // 新增Log
+    public static void AppendLog(string logMsg) {
+        using (StreamWriter w = File.AppendText("./record.log")) {
+            w.WriteLine("[{0}] {1}", DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss"), logMsg);
+        }
+    }
+
+    // revision 11
+    public class IniFile {
+        // doc. https://stackoverflow.com/questions/217902/reading-writing-an-ini-file
+
+        string Path;
+        string EXE = Assembly.GetExecutingAssembly().GetName().Name;
+
+        [DllImport("kernel32", CharSet = CharSet.Unicode)]
+        static extern long WritePrivateProfileString(string Section, string Key, string Value, string FilePath);
+
+        [DllImport("kernel32", CharSet = CharSet.Unicode)]
+        static extern int GetPrivateProfileString(string Section, string Key, string Default, StringBuilder RetVal, int Size, string FilePath);
+
+        public IniFile(string IniPath = null) {
+            Path = new FileInfo(IniPath ?? EXE + ".ini").FullName.ToString();
+        }
+
+        public string Read(string Key, string Section = null) {
+            var RetVal = new StringBuilder(255);
+            GetPrivateProfileString(Section ?? EXE, Key, "", RetVal, 255, Path);
+            return RetVal.ToString();
+        }
+
+        public void Write(string Key, string Value, string Section = null) {
+            WritePrivateProfileString(Section ?? EXE, Key, Value, Path);
+        }
+
+        public void DeleteKey(string Key, string Section = null) {
+            Write(Key, null, Section ?? EXE);
+        }
+
+        public void DeleteSection(string Section = null) {
+            Write(null, null, Section ?? EXE);
+        }
+
+        public bool KeyExists(string Key, string Section = null) {
+            return Read(Key, Section).Length > 0;
+        }
+    }
+}
---
+++ TempRecord/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace TempRecord {
+    class Program {
+        string urlCRM = "http://192.168.1.245/CRM";
+        string account;
+        string password;
+        string inputTemp;
+
+        static void Main(string[] args) {
+            Program p = new Program();            
+
+            // 載入帳密
+            FileUtil.IniFile ini = new FileUtil.IniFile("./Settings.ini");
+            p.account = ini.Read("Account");
+            p.password = ini.Read("Password");
+            p.inputTemp = ini.Read("Temperature");
+
+            // 溫度登錄流程
+            p.AutoRecordTemp();
+        }
+
+        void AutoRecordTemp() {
+            IWebDriver driver = new ChromeDriver();
+
+            // 開起官網
+            Console.WriteLine("開啟CRM：{0}", urlCRM);
+            driver.Navigate().GoToUrl(urlCRM);
+
+            // 輸入帳密
+            Console.WriteLine("輸入帳密\n帳號：{0}\n密碼：{1}", account, password);
+            var txtAccount = driver.FindElement(By.Id("Account"));
+            var txtPwd = driver.FindElement(By.Id("Password"));
+            txtAccount.SendKeys(account);
+            txtPwd.SendKeys(password);
+            Console.WriteLine("CRM登入");
+            txtPwd.SendKeys(OpenQA.Selenium.Keys.Enter);
+
+            try {
+                // 開啟體溫回報視窗
+                Console.WriteLine("開起溫度視窗");
+                var cmdTempRecord = driver.FindElement(By.Id("showCovid19_CONFIRM"));
+                cmdTempRecord.Click();
+
+                // 出勤狀況選擇
+                Console.WriteLine("輸入溫度");
+                var cboPeriods = driver.FindElement(By.Id("ddlCovid19_PERIODS"));
+                var periods = new SelectElement(cboPeriods);
+                periods.SelectByText("上班");
+                var cboAddend = driver.FindElement(By.Id("ddlCovid19_ATTEND"));
+                var attend = new SelectElement(cboAddend);
+                attend.SelectByText("正常出勤");
+
+                // 溫度輸入
+                var txtTemp = driver.FindElement(By.Id("txtCovid19_BODY_TEMPERATURE"));
+                txtTemp.SendKeys(inputTemp);
+
+                // 送出
+                Console.WriteLine("送出");
+                var cmdSubmit = driver.FindElement(By.XPath("//button[.='送出']"));
+                cmdSubmit.Click();
+
+                FileUtil.AppendLog("登錄溫度：" + inputTemp);
+                MessageBox.Show("溫度登錄完畢", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);    
+                
+            } catch (Exception ex) {
+                Console.WriteLine("失敗原因：" + ex.ToString());
+                FileUtil.AppendLog("登錄失敗");
+                MessageBox.Show("Fail", "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            } finally {
+                Console.WriteLine("關閉");
+                driver.Quit();
+            }
+        }
+    }
+}
